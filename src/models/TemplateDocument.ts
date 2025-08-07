9a75c495fccc7e855600e82b1a8456d64eb94a2a
mubashir <mubashir.ali@dror.co.in>
2025-08-07 13:29:17 +0530

    changes

--- src/models/TemplateDocument.ts
+++ src/models/TemplateDocument.ts
@@ -8,7 +8,7 @@ const TemplateDocumentSchema = new mongoose.Schema(
     name: { type: String, required: true },
     recipients: [
       {
-        name: { type: String, required: true },
+        name: { type: String, required: true, default: "" },
         email: { type: String, required: true },
       },
     ],
